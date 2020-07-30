7f2d101b97d097115a20050d84c4792f129c44e2
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-07-30 21:27:18 +0100

    Update RegNo.py

--- RegNo_Operations/RegNo.py
+++ RegNo_Operations/RegNo.py
@@ -1,23 +1,19 @@
+
 # Check Registrayion Number
 
-RegNo = "FE19A091"
+RegNo = "FE19A086"
 
 def isRegNo(RegNo):
 	if len(RegNo) != 8:
 		return False
-#	for i in range(0,2):
-#		if not RegNo[i].isapla():
-	#		return False
 	for i in range(2,4):
 		if not RegNo[i].isdecimal():
 			return False
-	if RegNo[4] != 'A' or RegNo[4] != 'B':
+	if RegNo[4] != 'A':
 		return False
-
 	for i in range(5,8):
 		if not RegNo[i].isdecimal():
 			return False
 	return True
 	
 print(isRegNo(RegNo))
-	
