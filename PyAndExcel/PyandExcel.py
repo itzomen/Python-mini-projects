87c6f4b76a9a11b67c97c2981259f6ec19fcb65e
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-08-03 20:40:10 +0100

    Update PyandExcel.py

--- PyAndExcel/PyandExcel.py
+++ PyAndExcel/PyandExcel.py
@@ -2,12 +2,17 @@
 #PyandExcel.py
 
 import openpyxl, pprint
+#install openpyxl
 
 
 print('Opening Student.xlsx...\n')
 wb = openpyxl.load_workbook('Students.xlsx')
-
+#opening the sheet having the data to be read
 sheet = wb['Student Data']
 studentsData = { }
 
 print('Reading Rows\n')
+for row in range(2, sheet.max_row + 1):
+	regNo = sheet['A' + str(row)].value
+	print(regNo +',' + str(sheet.max_row))
+
