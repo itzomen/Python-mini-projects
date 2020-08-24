e8312ac3e4a363ebecdcbb2f30ce33c7d423cb49
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-08-24 22:02:18 +0100

    Update currency_notifications.py

--- CurrencyNotifications/currency_notifications.py
+++ CurrencyNotifications/currency_notifications.py
@@ -1,18 +1,17 @@
 import requests, json
+from datetime import date
 
 #api to get currencies rates
-URL = 'https://www.freeforexapi.com/api/live?pairs=USDEUR'
+URL = 'https://www.freeforexapi.com/api/live?pairs=USDEUR, USDGBP'
 
-#getting the json
-response = requests.get(URL)
-if response:
-    print('Success!')
-else:
-    print('An error has occurred.')
-
-#saving fetched json into dictionary
-new_data = response.json()
-
-#saving new_data in json file
-with open('past_data.json', 'w') as file:
-	json.dump(new_data['rates'], file)
+def get_latest_rates():
+	day = date.today()
+	#getting the json
+	response = requests.get(URL)
+	if response:
+		print('Sucess!')
+	else:
+		print('Error getting Rates')
+	new_data = response.json()
+  #  with open('past_data.json', 'w') as file:
+    #	json.dump(new_data['rates'], file)
