07cc8d722669d5859b0e97ae71388c9b7124bdc0
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-08-28 21:30:03 +0100

    Added double display and new operators

--- Calvy/main.py
+++ Calvy/main.py
@@ -6,7 +6,7 @@ from kivy.uix.label import Label
 
 class MainApp(App):
     def build(self):
-        self.operators = ["/", "*", "+","%","^"]
+        self.operators = ["/", "*", "+","%","^","Mod"]
         self.last_was_operator = None
         self.last_button = None
         main_layout = BoxLayout(orientation="vertical")
@@ -17,9 +17,13 @@ class MainApp(App):
         #display
         self.calculation = TextInput(
             multiline=True, readonly=True, halign="right", font_size=250)
+        self.output = TextInput(
+            multiline=False, readonly=True, halign="right", font_size=250)
         main_layout.add_widget(self.calculation)
+        main_layout.add_widget(self.output)
         buttons = [
-            ["%", "^", "Del", "+"],
+            ["Mod","(",")","Ans"],
+            ["^", "%","Del","+"],
             ["7", "8", "9", "-"],
             ["4", "5", "6", "*"],
             ["1", "2", "3", "/"],
@@ -31,14 +35,13 @@ class MainApp(App):
                 if label == "=":
                 	button = Button(
                     text=label,
-                    background_color = (1, 0, 1, 1),
+                    background_color = (0, 1, 1, 1),
                     pos_hint={"center_x": 0.5, "center_y": 0.5})
                 	button.bind(on_press=self.calculate)
                 	h_layout.add_widget(button)
                 	continue
                 button = Button(
                     text=label,
-                    size_hint = (.9, .9),
                     pos_hint={"center_x": 0.4, "center_y": 0.4})
                 button.bind(on_press=self.on_button_press)
                 h_layout.add_widget(button)
@@ -54,6 +57,8 @@ class MainApp(App):
             self.calculation.text = ""
         elif button_text == "Del":
         	self.calculation.text = self.calculation.text[:-1]
+        elif button_text == "Ans":
+        	self.calculation.text += self.output.text
         else:
             if current and (
                 self.last_was_operator and button_text in self.operators):
@@ -72,11 +77,16 @@ class MainApp(App):
         text = self.calculation.text
         if text:
             try:
-            	self.calculation.text = self.calculation.text.replace("^", "**")
-            	calculation = str(eval(self.calculation.text))
-            	self.calculation.text = calculation
+            #replacing operators to python operators
+            	y = text.replace("^", "**")
+            	x = y.replace("%","*(0.01)*")
+            	self.output.text = x.replace("Mod","%")
+            	calculation = str(eval(self.output.text))
+            	self.output.text = calculation
+            	self.calculation.text = ""
             except Exception:
-            	self.calculation.text = "Error"
+            	self.output.text = "Error"
+            	self.calculation.text = ""
 
 
 if __name__ == "__main__":
