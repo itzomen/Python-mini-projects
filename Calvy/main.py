4aeeb35f6f74d860f3b0dd4e9714a65b01457493
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-08-28 09:24:39 +0100

    Update main.py
    Arrange the "=" button

--- Calvy/main.py
+++ Calvy/main.py
@@ -22,22 +22,18 @@ class MainApp(App):
         ]
         for row in buttons:
             h_layout = BoxLayout()
-            for label in row:
-                if label == "=":
-                	 equals_button = Button(
-                	 text="=", pos_hint={"center_x": 0.5, "center_y": 0.5})
-                	 equals_button.bind(on_press=self.on_solution)
-                	 main_layout.add_widget(equals_button)
-                	 continue
-                	 
+            for label in row:	 
                 button = Button(
                     text=label,
                     pos_hint={"center_x": 0.5, "center_y": 0.5},
                 )
+                if label == "=":
+                	button.bind(on_press=self.on_solution)
+                	h_layout.add_widget(button)
+                	continue
                 button.bind(on_press=self.on_button_press)
                 h_layout.add_widget(button)
             main_layout.add_widget(h_layout)
-
         return main_layout
 
     def on_button_press(self, instance):
